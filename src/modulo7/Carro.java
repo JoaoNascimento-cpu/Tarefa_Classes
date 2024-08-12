b73b83e9ba55eee82732bf5ec2f49e8c32e0fa09
João Nascimento <Joaoncs21@gmail.com>
2024-08-12 19:09:34 -0300

    Primeiro commit

---
+++ src/modulo7/Carro.java
@@ -0,0 +1,5 @@
+package modulo7;
+
+public class Carro {
+	
+}
---
+++ src/modulo7/Motorista.java
@@ -0,0 +1,37 @@
+package modulo7;
+
+public class Motorista {
+	int id;
+	
+	String Nome;
+	
+	String Endereco;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public String getEndereco() {
+		return Endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		Endereco = endereco;
+	}
+	
+	
+	
+	
+}
